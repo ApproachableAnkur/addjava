29e1d507722f46e7c2f5bad2d75efcfd5ff0d582
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-19 02:22:05 -0800

    version2

--- Addhello.java
+++ Addhello.java
@@ -3,9 +3,10 @@ class Addhello
 public static void main (String args[])
 {
 int a=20,b=30,c;
-c=a+b;
-
-System.out.println ("The addition of two number is :"+c);
+c=a*b;
+System.out.println ("The first number is :"+a);
+System.out.println ("The second number is :"+b);
+System.out.println ("The multiply of two number is :"+c);
 
 }
 
