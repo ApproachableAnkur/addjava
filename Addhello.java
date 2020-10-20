567b4a625b1721f3697ea04e36cf24f2a27e7650
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-19 21:53:02 -0800

    version4

--- Addhello.java
+++ Addhello.java
@@ -3,10 +3,10 @@ class Addhello
 public static void main (String args[])
 {
 int a=200,b=10,c;
-c=a-b;
+c=a*b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The Subtration of two number is :"+c);
+System.out.println ("The multiply of two number is :"+c);
 
 }
 
