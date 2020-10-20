de4ade1b56f2dff27cdf6921fb55b636933e7720
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-19 22:53:22 -0800

    version6

--- Addhello.java
+++ Addhello.java
@@ -6,7 +6,7 @@ int a=200,b=10,c;
 c=a+b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The add of two number is :"+c);
+System.out.println ("The result add of two number is :"+c);
 
 }
 
